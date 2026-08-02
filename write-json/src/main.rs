cc1e1a9c962ad4caa4239b191b729010bbef1d78
Mehul Prajapati <ailearn90@gmail.com>
2026-08-03 04:01:17 +0530

    write json

---
+++ write-json/src/main.rs
@@ -0,0 +1,32 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+struct Paragraph {
+    name: String
+}
+
+#[derive(Serialize, Deserialize)]
+struct Article {
+    article: String,
+    author: String,
+    paragraph: Vec<Paragraph>
+}
+
+fn main() {
+    let article: Article = Article {
+        article: String::from("My Article"),
+        author: String::from("John Doe"),
+        paragraph: vec![
+            Paragraph {
+                name: String::from("This is the first paragraph."),
+            },
+            Paragraph {
+                name: String::from("This is the second paragraph."),
+            },
+        ],
+    };
+
+    let json = serde_json::to_string(&article).unwrap();
+
+    println!("The JSON is: {}", json);
+}
