ea36ecbf0809f70f9c04b7e03d09893932062551
Mehul Prajapati <ailearn90@gmail.com>
2026-08-04 10:33:52 +0530

    actix with rhai

--- actix-api/src/main.rs
+++ actix-api/src/main.rs
@@ -1,25 +1,41 @@
-use actix_web::{
-    HttpServer,
-    Responder,
-    App,
-    get,
-    web::Path,
-};
+use actix_web::{App, HttpServer, Responder, get, web::Path};
 
-// use rhai::Engine;
+use rhai::Engine;
 
 #[get("/multiply/{num1}/{num2}")]
-async fn multiply(path: Path<(i32, i32)>) -> impl Responder {
+async fn multiply(path: Path<(i64, i64)>) -> impl Responder {
     let (num1, num2) = path.into_inner();
-    let result = num1 * num2;
-    format!("The result of multiplying {} and {} is: {}", num1, num2, result)
+
+    // create an instance of the rhai engine
+    let mut engine = Engine::new();
+
+    // register an API that exposes the numbers to Rhai
+    engine.register_fn("num1", move || num1);
+    engine.register_fn("num2", move || num2);
+
+    // run the script
+    let result = engine.eval_file::<i64>("src/multiply.rhai".into()).unwrap();
+
+    // return the result
+    format!("{result}")
 }
 
 #[get("/add/{num1}/{num2}")]
-async fn add(path: Path<(i32, i32)>) -> impl Responder {
+async fn add(path: Path<(i64, i64)>) -> impl Responder {
     let (num1, num2) = path.into_inner();
-    let result = num1 + num2;
-    format!("The result of adding {} and {} is: {}", num1, num2, result)
+
+    // create an instance of the rhai engine
+    let mut engine = Engine::new();
+
+    // register an API that exposes the numbers to Rhai
+    engine.register_fn("num1", move || num1);
+    engine.register_fn("num2", move || num2);
+
+    // run the script
+    let result = engine.eval_file::<i64>("src/add.rhai".into()).unwrap();
+
+    // return the result
+    format!("{result}")
 }
 
 #[get("/health")]
@@ -40,9 +56,9 @@ async fn main() -> std::io::Result<()> {
             .service(add)
             .service(health)
             .service(index)
-        })
-        .bind(("127.0.0.1", 8080))
-        .unwrap()
-        .run()
-        .await
+    })
+    .bind(("127.0.0.1", 8080))
+    .unwrap()
+    .run()
+    .await
 }
